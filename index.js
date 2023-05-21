828e970ba52f4c5013e473f5ff5f211c2261dbee
NusratJahanGeek <nusrat.jahan.4338@gmail.com>
2023-05-21 23:51:19 +0600

    Delete Functionality Added

--- index.js
+++ index.js
@@ -23,29 +23,68 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+
 
     const toyCollection = client.db('magicalToyland').collection('toys');
 
-    app.get('/toys', async(req, res) => {
+    app.get('/toys', async (req, res) => {
+      console.log(req.query.email);
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const result = await toyCollection.find(query).toArray();
+      res.send(result);
+    });
+
+
+    app.get('/toys', async (req, res) => {
       const cursor = toyCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
 
-    app.get('/toys/:id', async(req, res) => {
+
+    app.get('/toys/:id', async (req, res) => {
       const toyId = req.params.id;
       const objectId = new ObjectId(toyId)
       const toy = await toyCollection.findOne({ _id: objectId });
       res.send(toy);
     });
 
-    app.post('/toys', async(req, res) => {
-      const newToy = req.body;
-      console.log(newToy);
-      const result = await toyCollection.insertOne(newToy);
+    app.get('/toys/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await toyCollection.findOne(query);
       res.send(result);
     })
+
+    app.post('/toys', async (req, res) => {
+      const newToy = req.body;
+      const result = await toyCollection.insertOne(newToy);
+      res.send(result);
+    });
+
+    app.put('/toys/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const options = { upsert: true };
+      const updatedToy = req.body;
+      const toy = {
+        $set: {
+          quantity: updatedToy.quantity, details: updatedToy.details, price: updatedToy.price
+        }
+      }
+      const result = await toyCollection.updateOne(filter, toy, options);
+      res.send(result);
+    })
+
+    app.delete('/toys/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await toyCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -60,9 +99,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Magical ToyLand Is Running')
+  res.send('Magical ToyLand Is Running')
 })
 
 app.listen(port, () => {
-    console.log(`Magical ToyLand Server Is Running On Port: ${port}`)
+  console.log(`Magical ToyLand Server Is Running On Port: ${port}`)
 })
